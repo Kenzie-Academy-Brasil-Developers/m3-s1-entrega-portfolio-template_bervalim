13aea2b996f90c546c2219062f1924953280ec6b
bervalim <bernardogvalim@gmail.com>
2024-03-12 20:47:52 -0300

    fix/projects-styles

--- src/i18.js
+++ src/i18.js
@@ -20,11 +20,15 @@ const resources = {
       cadastroDevs: "Cadastro de desenvolvedores/tecnologia",
       restaurantApi: "Api para reservas em restaurantes",
       direitos: "Todos os direitos reservados",
-      project1Description: "Controle de estoque de e-commerce",
-      project2Description: "Aplicação de cadastro de clientes/contatos",
-      project3Description: "API para reservas em restaurantes",
+      project1Description:
+        "Neste projeto, criei um sistema de controle de estoque para e-commerce de roupas, com funcionalidades para administradores e usuários, desenvolvi  telas de cadastro, login, cadastros de produtos e telas de produtos específicos",
+      project2Description:
+        "Nesta aplicação de cadastro de clientes e contatos, consumi uma API, de minha autoria. No front-end, desenvolvi uma tela de cadastro de cliente, tela de login e uma dashboard onde o cliente é capaz de gerenciar os seus contatos (CRUD)",
+      project3Description:
+        "Esta API REST  é responsável por gerenciar os serviços de restaurantes. Através da aplicação, é possível realizar o cadastro de restaurantes e clientes interessados em realizar reservas em restaurantes. Além disso, é possível realizar a reserva e consultar os seus respectivos horários disponíveis no banco de dados do restaurante",
       project4Description:
-        "Aplicação de cadastro de desenvolvedores/tecnologia",
+        "Com esta aplicação, tive por objetivo desenvolver uma dashboard para desenvolvedores com as features de cadastro e login. Nesta dashboard, era possível que o desenvolvedor cadastrasse,editasse e deletasse as suas tecnologias.",
+
       Aplicacao: "Aplicação",
       Repositorio: "Repositório",
     },
@@ -47,10 +51,14 @@ const resources = {
       cadastroDevs: "Developer/technology registration",
       restaurantApi: "API for restaurant reservations",
       direitos: "All rights reserved",
-      project1Description: "Inventory control",
-      project2Description: "Customer/contact registration",
-      project3Description: "API for restaurant reservations",
-      project4Description: "Developer/technology registration",
+      project1Description:
+        "In this project, I created an inventory control system for clothing e-commerce, with features for administrators and users, I developed registration screens, login screens, product registrations and specific product screens",
+      project2Description:
+        "In this application for registering customers and contacts, I used an API of my own creation. On the front-end, I developed a customer registration screen, login screen and a dashboard where the customer is able to manage their contacts (CRUD)",
+      project3Description:
+        "This REST API is responsible for managing restaurant services. Through the application, it is possible to register restaurants and customers interested in making reservations at restaurants. In addition, you can make a reservation and check the respective available times in the restaurant's database.",
+      project4Description:
+        "With this application, I aimed to develop a dashboard for developers with registration and login features. In this dashboard, it was possible for the developer to register, edit and delete their technologies.",
       Aplicacao: "Application",
       Repositorio: "Repository",
     },
